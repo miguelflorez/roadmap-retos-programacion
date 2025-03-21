98d26d8819aa01656f429e4bac2bccbb3cea7e6d
miguelflorez <104113612+miguelflorez@users.noreply.github.com>
2025-03-21 16:10:41 -0500

    Create miguelflorez.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/miguelflorez.py
@@ -0,0 +1,32 @@
+# https://www.python.org/
+
+# Comentario en una linea 
+
+"""
+Comentario en varias lineas
+"""
+
+'''
+Otra forma de escribir 
+un comentario de varias 
+lineas 
+'''
+
+my_variable = " Mi variable "
+
+MY.CONSTANT = " Mi constante " 
+
+my_int = 3 
+my_float = 3.14
+my_bool = True 
+my_string = ' Mi cadena de texto '
+
+
+print ("¡Hola, python!")
+
+print (type(my_int))
+print (type(my_float))
+print (type(my_bool))
+print (type(my_string))
+
+
